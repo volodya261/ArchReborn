3ab5797f3225095bf2c3e0e5e34d968818b7b603
volodya261 <vova@gmail.com>
2025-03-21 14:05:16 +0400

    arch

--- conf/hypr/gruvbox/screenshot.sh
+++ conf/hypr/gruvbox/screenshot.sh
@@ -3,7 +3,7 @@
 temp_screenshot="/tmp/screenshot.png"
 save_file=$(date +'%y%m%d_%Hh%Mm%Ss_screenshot.png')
 
-grim -g "$(slurp)" - | wl-copy && wl-paste > $temp_screenshot | dunstify "Screenshot of the region taken" -t 1000 && swappy -f $temp_screenshot
+grim -g "$(slurp)" - | wl-copy && wl-paste > $temp_screenshot | dunstify "Screenshot of the region taken" -t 1000 && satty -f $temp_screenshot
 
 rm "$temp_screenshot"
 
