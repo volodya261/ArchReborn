a5f2f3c0c1faba2e7028e6bfbafaae68a0eff536
volodya <volodya@gmail.com>
2025-04-07 16:52:32 +0400

    arch

--- conf/nvim/lua/plugins/lsp-config.lua
+++ conf/nvim/lua/plugins/lsp-config.lua
@@ -1,24 +1,28 @@
 return {
   {
     "williamboman/mason.nvim",
+    lazy = false,
     config = function()
       require("mason").setup()
     end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "markdown_oxide" },
-      })
-    end,
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
   },
   {
     "neovim/nvim-lspconfig",
+    lazy = false,
     config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.markdown_oxide.setup({})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
