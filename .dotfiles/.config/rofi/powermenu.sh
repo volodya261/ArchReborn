f95da7a7cffad4595817810d80d3ba2d9553c333
volodya261 <rcurabsys@gmail.com>
2025-12-26 15:35:03 +0400

    cliphist

--- .dotfiles/.config/rofi/powermenu.sh
+++ .dotfiles/.config/rofi/powermenu.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
-# Список иконок (Nerd Font)
-#  Замок,  Выход,  Сон,  Ребут,  Выключение
-options="\n\n\n\n"
+# порядок: Выключение, Ребут, Сон, Выход, Замок
+options="\n\n\n\n"
 
-# Вызываем rofi в режиме dmenu
+# Вызываем rofi
 chosen="$(echo -e "$options" | rofi -dmenu -theme ~/.config/rofi/powermenu.rasi -p "Power Menu")"
 
-# Логика действий
+# Логика действий (теперь порядок в case не важен, главное совпадение символа)
 case $chosen in
-    "") hyprlock ;; # или swaylock / ваша команда блокировки
-    "") hyprctl dispatch exit ;;
-    "") systemctl suspend ;;
-    "") systemctl reboot ;;
     "") systemctl poweroff ;;
+    "") systemctl reboot ;;
+    "") systemctl suspend ;;
+    "") hyprctl dispatch exit ;;
+    "") hyprlock ;; # Убедитесь, что у вас установлен hyprlock или замените на свою команду
 esac
